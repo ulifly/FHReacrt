9454fbf4768aff18398ad45145056d3a0de6fd0b
Ulifly <ulifly@hotmail.com>
2023-11-09 19:29:26 -0600

    promises

--- 02-intro-javascript2/src/bases/08-imp-exp.js
+++ 02-intro-javascript2/src/bases/08-imp-exp.js
@@ -1,17 +1,19 @@
-import heroes, { owners } from './data/heroes';
+/* eslint-disable no-unused-vars */
+import heroes, { owners } from '../data/heroes';
 
 
 const getHeroesById = (id) => {
     return heroes.find(item => item.id === id);
-
 }
 
-console.log(getHeroesById(3));
+//console.log(getHeroesById(3));
 
 const getHeroesByOwner = (owner) => {
     return heroes.filter(heroe => heroe.owner === owner);
 }
 
-console.log( getHeroesByOwner( 'DC' ) );
+// console.log( getHeroesByOwner( 'DC' ) );
 
-console.log( owners );
+// console.log( owners );
+
+export { getHeroesById, getHeroesByOwner };
--- 02-intro-javascript2/src/index.js
+++ 02-intro-javascript2/src/index.js
@@ -1,1 +1,13 @@
-console.log(' cara de perro ');
+import { getHeroesById } from "./bases/08-imp-exp";
+
+const nuevaPromesa = new Promise((resolve, reject) => {
+    setTimeout( () => {
+        const heroe = getHeroesById(3);
+        console.log(heroe);
+        resolve();
+    }, 2000 );
+})
+
+nuevaPromesa.then( () => {
+    console.log('Todo ok then');
+})
